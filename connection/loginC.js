210b5609a3491f90f3558f5436cbd4fd47f54ee1
m2nja201 <m2nja201@gmail.com>
2021-08-15 15:00:06 +0900

    8/15 2:59 PM commit(js file)

---
+++ connection/loginC.js
@@ -0,0 +1,27 @@
+function sendRequest() {
+  var id = document.login_form.userID.value;
+
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (
+      httpRequest.readyState == XMLHttpRequest.DONE &&
+      httpRequest.status == 200
+    ) {
+      document.getElementById("text").innerHTML = httpRequest.responseText;
+    }
+  };
+  // POST 방식의 요청은 데이터를 Http 헤더에 포함시켜 전송함.
+  httpRequest.open(
+    "POST",
+    `http://115.85.181.34:8081/api/user/login?userPhoneNum=1&userPw=1234`,
+    true
+  );
+  httpRequest.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  httpRequest.send(
+    "creationTime=11:11:11&id=minjae12&lastAccessedTime=22:22:22"
+  );
+}
+console.log(id);
