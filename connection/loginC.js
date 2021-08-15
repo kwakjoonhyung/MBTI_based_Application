55ca038a8c352d054ce4cd328c2685da307aef16
kyw0716 <77326660+kyw0716@users.noreply.github.com>
2021-08-15 16:36:15 +0900

    Update loginC.js

--- connection/loginC.js
+++ connection/loginC.js
@@ -1,21 +1,28 @@
-var id = document.getElementById("userID");
-
-console.log(id);
+// function logging(){
+//   let st_name = document.getElementById('st_name').value
+//   let st_id = document.getElementById('st_id').value
+//   let department = document.getElementById('department').value
+//   console.log(`이름 ${st_name} 아이디 ${st_id} 학과 ${department}`)
+// }
 
 function sendRequest() {
+  let userID = document.getElementById('userID').value
+  let userPW = document.getElementById('userPW').value
+  console.log(`아이디 ${userID} 비밀번호 ${userPW}`)
+
   var httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function () {
     if (
       httpRequest.readyState == XMLHttpRequest.DONE &&
       httpRequest.status == 200
     ) {
-      document.getElementById("text").innerHTML = httpRequest.responseText;
+      // document.getElementById("text").innerHTML = httpRequest.responseText;
     }
   };
   // POST 방식의 요청은 데이터를 Http 헤더에 포함시켜 전송함.
   httpRequest.open(
     "POST",
-    `http://115.85.181.34:8081/api/user/login?userPhoneNum=1&userPw=1234`,
+    `http://115.85.181.34:8081/api/user/login?userPhoneNum=${userID}&userPw=${userPW}`,
     true
   );
   httpRequest.setRequestHeader(
@@ -23,6 +30,6 @@ function sendRequest() {
     "application/x-www-form-urlencoded"
   );
   httpRequest.send(
-    "creationTime=11:11:11&id=minjae12&lastAccessedTime=22:22:22"
+    ""
   );
 }
